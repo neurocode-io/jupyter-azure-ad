0cab6573c45618fd697a27befb20cac547894fb0
Bobby Donchev <contact@donchev.is>
2020-11-22 16:27:43 +0100

    Add startup script

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
-docker run --net=host --rm --init -it quay.io/oauth2-proxy/oauth2-proxy:v6.1.1 --client-id '************************' \
+#!/bin/bash
+
+set -e
+
+if ! command -v docker &> /dev/null
+then
+    echo "docker missing? Shouldnt happen..."
+    exit
+fi
+
+docker run --rm -d --net=host donchev7/jupyter-cuda-torch:6.5.1
+
+docker run --net=host --rm --init -d quay.io/oauth2-proxy/oauth2-proxy:v6.1.1 --client-id '************************' \
     --client-secret '*************************' \
     --cookie-secret $(openssl rand -hex 16) \
     --provider \
@@ -16,4 +28,5 @@ docker run --net=host --rm --init -it quay.io/oauth2-proxy/oauth2-proxy:v6.1.1 -
     --http-address \
     0.0.0.0:8080 \
     --upstream \
-    http://localhost:5601
+    http://localhost:8888
+
